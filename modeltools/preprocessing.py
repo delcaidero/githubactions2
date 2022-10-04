4e449f1f2febe500a0d43f12068bffd90ee4452f
Alby <alby.mendoza@gmail.com>
2022-10-04 20:42:26 +0000

    añadido test en documentacion

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -5,4 +5,24 @@ este módulo contiene helpers par ael prerpocesamiento de datos
 import numpy as np
 
 def get_numerical_features(df):
+    """
+    devuelve una lista con el nombre de las coumnas que d
+
+    Parámetros
+    ----------
+
+
+
+    Ejemplos
+    --------
+
+    >>> from modeltools.preprocessing import get_numerical_features
+    >>> import pandas as pd
+    >>> df = pd.DataFrame({'a':[1]})
+    >>> get_numerical_features(df)
+    ['a']
+
+
+
+    """
     return list(df.select_dtypes(include=[np.number]).columns)
